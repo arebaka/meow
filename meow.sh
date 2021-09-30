ed0779fdca391a6da7a2965b0877f224d3c22816
arelive <me@are.moe>
2021-09-30 13:14:07 +0300

    init

---
+++ meow.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+VERSION="0.1"
+
+
+
+
+error() {
+	echo $1 >&2
+	exit $2
+}
+
+usage() {
+	cat >&2 <<EOF
+Usage: $0 [options...] <method> <URL>
+Type $0 --help for more information
+EOF
+
+	exit 1
+}
+
+help() {
+	cat <<EOF
+meow v$VERSION by arelive <me@are.moe>
+An easy tool for making requests using cURL and following redirects
+
+Usage: $0 [options...] <method> <URL>
+When the request can have a body (methods POST, PUT, DELETE, PATCH), then it is read from stdin.
+When a successful response has a body, then it is written to stdout.
+If the response cannot has a body (methods HEAD, PUT, TRACE), then its headers is written to stdout.
+If cURL returns a non-zero exit code, its output is written to stderr without changes.
+Methods can be specified in any case (post, POST, POsT, etc).
+
+Allowed methods:
+  get
+  head
+  post
+  put
+  delete
+  connect
+  options
+  trace
+  patch
+
+Options:
+  -a, --useragent   "<useragent>"    - set a user-agent for the request
+  -c, --cookie      "<cookie>"       - set a cookie for the request
+  -C, --save-cookie <filename>       - save a cookie from the response to the file
+  -h, --header      "<key>: <value>" - set a optional header for the request
+  -H, --get-headers                  - get response headers instead of body
+  -p, --proxy       <proxy>          - use the proxy
+  -r, --referrer    <referrer>       - set an address making the request
+  -t, --type        <type>           - set a type of the sending content
+  -v, --version                      - show a version of the tool
+EOF
+
+	exit 0
+}
+
+request() {
+	local method=$1
+	local url=$2
+	local useragent=${3:-"meow v$VERSION by arelive (https://are.moe)"}
+	local cookie=$4
+	local save_cookie=$5
+	local proxy=$6
+	local referrer=$7
+	local type=${8:-"application/octet-stream"}
+	local get_headers=$9
+	local headers=""
+
+	for h in ${10}
+	do headers="$headers -H $h"
+	done
+
+	case $method in
+		POST|PUT|DELETE|PATCH)
+			curl -sSL -X $method \
+				-A "$useragent" \
+				-H "Cached-Control: no-cache" \
+				-H "Cookie: $cookie" \
+				-H "Content-Type: $type" \
+				$([ -n "$save_cookie" ] && cat<<<"-b $save_cookie") \
+				$([ -n "$proxy" ] && cat<<<"-x $proxy") \
+				$([ -n "$referrer" ] && cat<<<"-e $referrer") \
+				$([ -n "$get_headers" ] && cat<<<"-I") \
+				$headers \
+				--data-binary @- "$url"
+		;;
+		GET|HEAD|CONNECT|OPTIONS|TRACE)
+			curl -sSL -X $method \
+				-A "$useragent" \
+				-H "Cached-Control: no-cache" \
+				-H "Cookie: $cookie" \
+				$([ -n "$save_cookie" ] && cat<<<"-b $save_cookie") \
+				$([ -n "$proxy" ] && cat<<<"-x $proxy") \
+				$([ -n "$referrer" ] && cat<<<"-e $referrer") \
+				$([ -n "$get_headers" ] && cat<<<"-I") \
+				$headers \
+				"$url"
+		;;
+		*)
+			error "The method $method not allowed!" 1
+		;;
+	esac
+
+	exit $?
+}
+
+
+
+
+posargs=()
+headers=()
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		--help)
+			help
+		;;
+		-v|--version)
+			echo $VERSION
+			exit 0
+		;;
+		-a|--useragent|-c|--cookie|-C|--save-cookie|-h|--header|-p|--proxy|-r|--referrer|-t|--type)
+			[ $# -lt 2 ] && error "The option -$1 requires a parameter!" 1
+
+			case $1 in
+				-a|--useragent)    useragent=$2   ;;
+				-c|--cookie)       cookie=$2      ;;
+				-C|--save-cookie)  save_cookie=$2 ;;
+				-h|--header)       headers+=($2)  ;;
+				-p|--proxy)        proxy=$2       ;;
+				-r|--referrer)     referrer=$2    ;;
+				-t|--type)         type=$2        ;;
+			esac
+
+			shift; shift
+		;;
+		-H|--get-headers)
+			get_headers=1
+			shift
+		;;
+		*)
+			posargs+=($1)
+			shift
+		;;
+	esac
+done
+
+[ ${#posargs[@]} -ne 2 ] && usage
+
+method=${posargs[0]}
+url=${posargs[1]}
+
+
+
+
+request ${method^^} "$url" "$useragent" "$cookie" "$save_cookie" "$proxy" "$referrer" "$type" "$get_headers" "${headers[*]}"
